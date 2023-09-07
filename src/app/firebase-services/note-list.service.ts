01ef3d53b37042bdf251eaa99d978b6f874b174a
BayerTobias <tbayer2@gmx.de>
2023-09-07 21:44:01 +0200

    onSnapshot

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -4,8 +4,10 @@ import {
   collection,
   doc,
   collectionData,
+  onSnapshot,
 } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { Note } from '../interfaces/note.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -13,18 +15,40 @@ import { Observable } from 'rxjs';
 export class NoteListService {
   firestore: Firestore = inject(Firestore);
 
-  items$: any;
-  items: any;
+  // items$: any;
+  // items: any;
+
+  unsubNotes;
+
+  notes: Note[] = [];
+  trash: Note[] = [];
 
   constructor() {
-    this.items$ = collectionData(this.getNotesRef());
-    this.items = this.items$.subscribe((list: any) => {
-      console.log(list);
+    this.unsubNotes = onSnapshot(this.getNotesRef(), (list) => {
+      list.forEach((element) => {
+        const data = element.data();
+        console.log(data['title']);
+      });
     });
+
+    // this.items$ = collectionData(this.getNotesRef());
+    // this.items = this.items$.subscribe((list: any) => {
+    //   console.log(list);});
   }
 
   ngonDestroy() {
-    this.items.unsubscribe();
+    // this.items.unsubscribe();
+    this.unsubNotes();
+  }
+
+  setNoteObject(obj: any, id: string): Note {
+    return {
+      id: id,
+      type: obj.type || 'note',
+      titel: obj.title || '',
+      content: obj.description || '',
+      marked: obj.marked || false,
+    };
   }
 
   getNotesRef() {
